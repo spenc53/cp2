60b6d3482990121c113302a79fa6b1f29470fb4c
Spencer Seeger <seeger.spencer@gmail.com>
2017-02-02 22:35:09 -0700

    More Stuff

--- NODEJS_CHAT/index.js
+++ NODEJS_CHAT/index.js
@@ -1,8 +1,18 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var dir = '/HTML/';
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + dir + 'index.html');
+});
+
+io.on('connection', function(socket){
+    socket.on('chat', function(data){
+        console.log(data);
+        console.log(data.user +": " + data.message);
+        io.emit('chat',data);
+    });
 });
 
 http.listen(3000, function(){
